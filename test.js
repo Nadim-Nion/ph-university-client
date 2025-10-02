661b5a69462f7b185079dd58a9b2a8784a02b9eb
Nadim-Nion <nadim15-1746@diu.edu.bd>
2025-10-02 19:17:15 +0600

    array reduce recap

---
+++ test.js
@@ -0,0 +1,45 @@
+/* 
+const array = [1,2,3,4];
+
+const result = array.reduce((total, item) => {
+  total.push(total + item);
+   return total;
+}, []);
+
+console.log("Final result:", result);
+*/
+
+const adminpaths2 = [
+  {
+    name: "Dashboard",
+    path: "/admin/dashboard",
+    element: "ADMIN_DASHBOARD",
+  },
+  {
+    name: "User Management",
+    children: [
+      {
+        name: "Create Admin",
+        path: "/admin/create-admin",
+        element: "CREATE_ADMIN",
+      },
+      {
+        name: "Create Faculty",
+        path: "/admin/create-faculty",
+        element: "CREATE_FACULTY",
+      },
+      {
+        name: "Create Student",
+        path: "/admin/create-student",
+        element: "CREATE_STUDENT",
+      },
+    ],
+  },
+];
+
+const newArray = adminpaths2.reduce((acc, item) => {
+    acc.push(item);
+    return acc;
+}, []);
+
+console.log(newArray)
