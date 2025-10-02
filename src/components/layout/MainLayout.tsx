2a8f559474ede947eb5074d30b29c26d7e5aa582
Nadim-Nion <nadim15-1746@diu.edu.bd>
2025-10-02 11:05:40 +0600

    a centralized array for routes and sidebar items

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { Layout, Menu, type MenuProps } from "antd";
-import { Outlet } from "react-router";
+import { NavLink, Outlet } from "react-router";
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -16,24 +16,24 @@ const { Header, Content, Footer, Sider } = Layout;
 
 const items: MenuProps["items"] = [
   {
-    key: "1",
-    label: "Dashboard",
+    key: "Dashboard",
+    label: <NavLink to="/admin/dashboard">Dashboard</NavLink>,
   },
   {
-    key: "2",
-    label: "Profile",
-  },
-  {
-    key: "3",
+    key: "User Management",
     label: "User Management",
     children: [
       {
-        key: "3-1",
-        label: "Create Admin",
+        key: "Create Admin",
+        label: <NavLink to="/admin/create-admin">Create Admin</NavLink>,
       },
       {
-        key: "3-2",
-        label: "Create Student",
+        key: "Create Faculty",
+        label: <NavLink to="/admin/create-faculty">Create Faculty</NavLink>,
+      },
+      {
+        key: "Create Student",
+        label: <NavLink to="/admin/create-student">Create Student</NavLink>,
       },
     ],
   },
