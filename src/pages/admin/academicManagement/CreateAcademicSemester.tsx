120c141923dc7b24ee9fe27fcb110224e4f74fba
Nadim-Nion <nadim15-1746@diu.edu.bd>
2025-11-26 18:49:22 +0600

    setting up the next steps for user management

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,7 +1,19 @@
+import type { FieldValues, SubmitHandler } from "react-hook-form";
+import PHForm from "../../../components/form/PHForm";
+import PHInput from "../../../components/form/PHInput";
+import { Button } from "antd";
+
 const CreateAcademicSemester = () => {
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log("data in create academic semester:", data);
+  };
+
   return (
     <div>
-      <h2>Create Academic Semester</h2>
+      <PHForm onSubmit={onSubmit}>
+        <PHInput type="text" name="name" />
+        <Button htmlType="submit">Submit</Button>
+      </PHForm>
     </div>
   );
 };
