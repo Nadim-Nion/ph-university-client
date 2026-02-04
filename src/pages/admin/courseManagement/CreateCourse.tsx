f524cb8e8d6f252b8ffcf6b2aac1137ebef96c58
Nadim-Nion <nadim15-1746@diu.edu.bd>
2026-02-04 11:37:53 +0600

    course managment sidebar added

---
+++ src/pages/admin/courseManagement/CreateCourse.tsx
@@ -0,0 +1,9 @@
+const CreateCourse = () => {
+  return (
+    <div>
+      <h2>Create Course</h2>
+    </div>
+  );
+};
+
+export default CreateCourse;
---
+++ src/pages/admin/courseManagement/OfferedCourses.tsx
@@ -0,0 +1,9 @@
+const OfferedCourses = () => {
+  return (
+    <div>
+      <h2>Offered Courses</h2>
+    </div>
+  );
+};
+
+export default OfferedCourses;
